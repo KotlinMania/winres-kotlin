27e2d98929a64757a878ac850f28f0c3623ccf41
Sydney Renee <sydney@solace.ofharmony.ai>
2026-08-12 09:11:49 -0700

    style: fix ktlint multiline expression violations
    ktlint requires multiline expressions to start on a new line.
    Applied ktlintFormat to WindowsResource.kt and WindowsResourceTest.kt
    to resolve all indentation and multiline expression violations.

--- src/commonTest/kotlin/io/github/kotlinmania/winres/WindowsResourceTest.kt
+++ src/commonTest/kotlin/io/github/kotlinmania/winres/WindowsResourceTest.kt
@@ -15,17 +15,19 @@ class WindowsResourceTest {
 
     @Test
     fun toolkitInlcudeWin10() {
-        val result = winSdkInlcudeRoot(
-            "C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.17763.0\\x64\\rc.exe",
-        )
+        val result =
+            winSdkInlcudeRoot(
+                "C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.17763.0\\x64\\rc.exe",
+            )
         assertEquals("C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.17763.0", result)
     }
 
     @Test
     fun toolkitInlcudeWin8() {
-        val result = winSdkInlcudeRoot(
-            "C:\\Program Files (x86)\\Windows Kits\\8.1\\bin\\x86\\rc.exe",
-        )
+        val result =
+            winSdkInlcudeRoot(
+                "C:\\Program Files (x86)\\Windows Kits\\8.1\\bin\\x86\\rc.exe",
+            )
         assertEquals("C:\\Program Files (x86)\\Windows Kits\\8.1\\Include", result)
     }
 }
